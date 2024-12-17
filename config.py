15c43c8e3a36cb9733fa2c0dfab78bdfb2ef7bcd
Ephemeral2333 <liyh_0703@foxmail.com>
2024-12-17 14:30:39 +0800

    update config.py

--- config.py
+++ config.py
@@ -13,13 +13,11 @@ save_freq = 2
 val_freq = 2
 start_save_epoch = 2
 
-train_data_dir = '/root/autodl-tmp/HUGO4_dataset/train/'
-val_data_dir = '/root/autodl-tmp/HUGO4_dataset/validation/'
-# train_data_dir = 'D:\\File\\Graduation Design\\dataset\\wow7_dataset\\train'
-# val_data_dir = 'D:\\File\\Graduation Design\\dataset\\wow7_dataset\\validation'
-test_data_dir = '/root/autodl-tmp/wow1_dataset/test/'
+train_data_dir = './data/wow_1.0/train/'
+val_data_dir = './data/wow_1.0/validation/'
+test_data_dir = './data/wow_1.0/test/'
 stego_img_height = 256  # stego_img_height == stego_img_width 长宽
 stego_img_channel = 3  # 彩色3 灰色1
 
-pre_trained_srnet_path = '/root/autodl-tmp/srnet_02/checkpoints/SRNet/checkpoint_hugo4.pt'
+pre_trained_srnet_path = './checkpoints/SRNet_CBAM/checkpoint_hugo4.pt'
 # pre_trained_srnet_path = None
